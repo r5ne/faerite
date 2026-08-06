7e91aeb92be0463de43db1ab86989e71ebfd0ccb
r5ne <renetimantsev@gmail.com>
2026-08-06 05:20:34 +0100

    Added main class.

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+void main() {
+    Region britishIsles = new Region("British Isles", "archipelago", "000000", null, null);
+    System.out.println(britishIsles.name());
+    javafx.application.Application.launch(MapView.class);
+}
