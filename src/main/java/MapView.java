af9577f575df1022ad3ef6d4696696bc0f221e85
r5ne <renetimantsev@gmail.com>
2026-08-09 00:17:31 +0100

    Made MapView the Pane of the actual Map rather than the whole javafx runtime. Added the ability to load and arrange map tiles so that large maps can be loaded without running into RAM limits. Added map scaling calculation to the layoutChildren method so that the Map is correctly scaled to the screen no matter the size whenever the window is resized.

--- src/main/java/MapView.java
+++ src/main/java/MapView.java
@@ -1,17 +1,66 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.transform.Scale;
 
-public class MapView extends Application {
+public class MapView extends Pane {
+    private final MapViewModel viewModel = new MapViewModel();
+
+    private final Group mapTiles;
+    private final Scale mapScale = new Scale();
+
+    public MapView() {
+        backgroundProperty().bind(Bindings.createObjectBinding(() ->
+            new Background(new BackgroundFill(viewModel.getOceanColor().get(), CornerRadii.EMPTY, Insets.EMPTY)),
+                viewModel.getOceanColor()
+        ));
+
+        mapTiles = assembleTiles("tile", 6, 4096);
+        mapTiles.getTransforms().add(mapScale);
+        getChildren().add(mapTiles);
+    }
+
     @Override
-    public void start(Stage stage) {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root);
+    protected void layoutChildren() {
+        super.layoutChildren();
 
-        stage.setTitle("Faerite");
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.show();
+        double paneWidth = getWidth();
+        double paneHeight = getHeight();
+        double mapTilesWidth = mapTiles.getBoundsInLocal().getWidth();
+        double mapTilesHeight = mapTiles.getBoundsInLocal().getHeight();
+
+        if (paneWidth <= 0 || paneHeight <= 0 || mapTilesWidth <= 0 || mapTilesHeight <= 0) {
+            return;
+        }
+
+        double newScale = Math.min(paneWidth / mapTilesWidth, paneHeight / mapTilesHeight);
+        mapScale.setX(newScale);
+        mapScale.setY(newScale);
+
+        double scaledWidth = mapTilesWidth * newScale;
+        double scaledHeight = mapTilesHeight * newScale;
+        mapTiles.setTranslateX((paneWidth - scaledWidth) / 2);
+        mapTiles.setTranslateY((paneHeight - scaledHeight) / 2);
+    }
+
+    private Group assembleTiles(String tileName, int sideLength, int tileSize) {
+        Group tileGroup = new Group();
+        for (int x = 0; x < sideLength; x++) {
+            for (int y = 0; y < sideLength; y++) {
+                String path = "/" + tileName + "_" + x + "_" + y + ".png";
+                Image image = new Image(getClass().getResourceAsStream(path));
+                ImageView tile = new ImageView(image);
+
+                tile.setX(x * tileSize);
+                tile.setY(y * tileSize);
+                tile.setSmooth(false);
+
+                tileGroup.getChildren().add(tile);
+            }
+        }
+        return tileGroup;
     }
 }
