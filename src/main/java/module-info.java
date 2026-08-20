0b770dc3ee9364c8cd7d4ce1e912a464cd563c15
r5ne <renetimantsev@gmail.com>
2026-08-20 03:08:48 +0100

    added jackson for loading from json files.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module faerite {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
+    requires com.fasterxml.jackson.databind;
 
     exports faerite;
     exports faerite.model;
