01376185335a08318fd68e9de9861e991d2f8506
r5ne <renetimantsev@gmail.com>
2026-08-22 02:48:24 +0100

    Added jetbrains Annotations for @Nullable support.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,10 @@ module faerite {
     requires javafx.controls;
     requires javafx.graphics;
     requires com.fasterxml.jackson.databind;
+    requires org.jetbrains.annotations;
 
     exports faerite;
     exports faerite.model;
     exports faerite.view;
+    exports faerite.viewmodel;
 }
