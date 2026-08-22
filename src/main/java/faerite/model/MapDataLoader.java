aa3ba17938605b13692a25cf72f1792d7d994142
r5ne <renetimantsev@gmail.com>
2026-08-22 02:52:19 +0100

    Removed the explicit single file name constant in preparation for loading multiple mapmodels at runtime.

--- src/main/java/faerite/model/MapDataLoader.java
+++ src/main/java/faerite/model/MapDataLoader.java
@@ -8,13 +8,12 @@ import java.nio.file.Path;
 
 public final class MapDataLoader {
 
-    private static final String MAP_MODEL_FILE_NAME = "british-isles.json";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     private MapDataLoader() {}
 
-    public static MapModel loadMapModel() {
-        Path filePath = Path.of("src/main/resources/mapdata/" + MAP_MODEL_FILE_NAME);
+    public static MapModel loadMapModel(String mapModelFileName) {
+        Path filePath = Path.of("src/main/resources/mapdata/" + mapModelFileName);
         try (var reader = Files.newBufferedReader(filePath)) {
             return objectMapper.readValue(reader, MapModel.class);
         } catch (FileNotFoundException e) {
