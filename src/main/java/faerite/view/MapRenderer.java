e42067eb7a23964f5287ed80742beaffea810204
r5ne <renetimantsev@gmail.com>
2026-08-24 18:07:15 +0100

    Fixed setting properties in the MapRenderer not repainting the map. Made the paintComponent method safer against nulls and using a standard Swing recommended try finally loop for disposing graphics contexts after use.

--- src/main/java/faerite/view/MapRenderer.java
+++ src/main/java/faerite/view/MapRenderer.java
@@ -1,13 +1,17 @@
 package faerite.view;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import javax.swing.*;
 
 public final class MapRenderer extends JPanel {
-    private BufferedImage mapImage;
 
-    private double zoomFactor;
+    private BufferedImage mapImage;
+    private double zoomFactor = 1.0;
+
+    public MapRenderer() {
+        setOpaque(true);
+    }
 
     public void setMapImage(BufferedImage mapImage) {
         this.mapImage = mapImage;
@@ -16,22 +20,35 @@ public final class MapRenderer extends JPanel {
 
     public void setZoomFactor(double zoomFactor) {
         this.zoomFactor = zoomFactor;
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+        if (mapImage == null) {
+            return;
+        }
 
-        int scaledWidth = (int) (mapImage.getWidth() * zoomFactor);
-        int scaledHeight = (int) (mapImage.getHeight() * zoomFactor);
+        Graphics2D g2 = (Graphics2D) g.create();
 
-        int drawX = (getWidth() - scaledWidth) / 2;
-        int drawY = (getHeight() - scaledHeight) / 2;
+        try {
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
 
-        g2D.drawImage(mapImage, drawX, drawY, scaledWidth, scaledHeight, null);
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
+            int scaledWidth = (int) Math.round(mapImage.getWidth() * zoomFactor);
+
+            int scaledHeight = (int) Math.round(mapImage.getHeight() * zoomFactor);
+
+            int drawX = (getWidth() - scaledWidth) / 2;
+
+            int drawY = (getHeight() - scaledHeight) / 2;
+
+            g2.drawImage(mapImage, drawX, drawY, scaledWidth, scaledHeight, null);
+        } finally {
+            g2.dispose();
+        }
     }
 }
