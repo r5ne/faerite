4ff1eba8358c4e9be48cfc1ad24477883c61fd71
r5ne <renetimantsev@gmail.com>
2026-08-24 17:18:20 +0100

    Implemented the MapRenderer planned interface. Currently it simply attempts to display a loaded BufferImage and position it on the screen usign the given scale.

--- src/main/java/faerite/view/MapRenderer.java
+++ src/main/java/faerite/view/MapRenderer.java
@@ -1,6 +1,36 @@
 package faerite.view;
 
-public interface MapRenderer {
-    void setRenderState(MapRenderState state);
-    void setRenderSize(int width, int height);
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public final class MapRenderer extends JPanel {
+    private BufferedImage mapImage;
+
+    private double zoomFactor;
+
+    public void setRenderState(MapRenderState state) {
+        mapImage = state.mapImage();
+    }
+
+    public void setZoomFactor(double zoomFactor) {
+        this.zoomFactor = zoomFactor;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+
+        int scaledWidth = (int) (mapImage.getWidth() * zoomFactor);
+        int scaledHeight = (int) (mapImage.getHeight() * zoomFactor);
+
+        int drawX = (getWidth() - scaledWidth) / 2;
+        int drawY = (getHeight() - scaledHeight) / 2;
+
+        g2D.drawImage(mapImage, drawX, drawY, scaledWidth, scaledHeight, null);
+    }
 }
