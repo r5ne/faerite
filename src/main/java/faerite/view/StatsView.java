257710730f2208addfeac628041131567fbdb7bd
r5ne <renetimantsev@gmail.com>
2026-08-25 01:56:35 +0100

    Reimplemented updating sidebar based on RegionData.

--- src/main/java/faerite/view/StatsView.java
+++ src/main/java/faerite/view/StatsView.java
@@ -3,7 +3,8 @@ package faerite.view;
 import faerite.model.RegionData;
 import faerite.model.RegionSelectionModel;
 import faerite.viewmodel.AtlasViewModel;
-import faerite.model.RegionData;
+import faerite.viewmodel.MapViewModel;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -14,23 +15,38 @@ public class StatsView extends VBox {
     private final Label titleLabel = new Label();
     private final Label typeLabel = new Label();
 
+    private final ChangeListener<RegionSelectionModel> selectedRegionListener;
+
     public StatsView(AtlasViewModel viewModel) {
         this.viewModel = viewModel;
 
         titleLabel.getStyleClass().add("h1");
         typeLabel.getStyleClass().add("body-text");
 
-        // temporary
-        // viewModel.getCurrentRegionDataProperty().addListener((_, _, newData) -> updateLabels(newData));
-        // updateLabels(viewModel.getCurrentMap().regionData());
+        selectedRegionListener = (_, _, newRegion) -> updateLabels(newRegion);
 
         getChildren().addAll(titleLabel, typeLabel);
+
+        viewModel.activeLayerProperty().addListener((_, oldMap, newMap) -> updateListeners(oldMap, newMap));
+        updateListeners(null, viewModel.getActiveLayer());
     }
 
-    private void updateLabels(RegionData data) {
-        if (data != null) {
-            titleLabel.setText(data.name());
-            typeLabel.setText(String.format("Type: %s", data.type().getDisplayName()));
+    private void updateLabels(RegionSelectionModel newRegion) {
+        RegionData regionData;
+        if (newRegion != null) {
+            regionData = newRegion.regionData();
+        } else {
+            regionData = viewModel.getActiveLayer().mapModel.regionData();
         }
+        titleLabel.setText(regionData.name());
+        typeLabel.setText(String.format("Type: %s", regionData.type().getDisplayName()));
+    }
+
+    private void updateListeners(MapViewModel oldMap, MapViewModel newMap) {
+        if (oldMap != null) {
+            oldMap.getSelectedRegionProperty().removeListener(selectedRegionListener);
+        }
+        newMap.getSelectedRegionProperty().addListener(selectedRegionListener);
+        updateLabels(newMap.getSelectedRegion());
     }
 }
