01ccafa737934f45eceec8227fd900be967b4fcb
r5ne <renetimantsev@gmail.com>
2026-08-27 04:00:22 +0100

    Updated missed InfoView to OverlayView.

--- src/main/java/faerite/atlas/overlay/RootView.java
+++ src/main/java/faerite/atlas/overlay/RootView.java
@@ -11,7 +11,7 @@ public class RootView extends StackPane {
     /// @param viewModel The global view model to pass to all other panes.
     public RootView(AtlasViewModel viewModel) {
         MapView map = new MapView(viewModel);
-        InfoView info = new InfoView(viewModel);
+        OverlayView info = new OverlayView(viewModel);
         this.getChildren().addAll(map, info);
     }
 }
