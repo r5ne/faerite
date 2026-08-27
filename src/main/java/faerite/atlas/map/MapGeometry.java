5a7ced9208e9cb9a1e2c44b0cf8ae78ba1b09fda
r5ne <renetimantsev@gmail.com>
2026-08-27 03:57:14 +0100

    Moved pixel checking into its own class since that has nothing to do with MapGeometry

--- src/main/java/faerite/atlas/map/MapGeometry.java
+++ src/main/java/faerite/atlas/map/MapGeometry.java
@@ -35,19 +35,4 @@ public final class MapGeometry {
 
         return new Point(pixelX, pixelY);
     }
-
-    /// Gets the color of a pixel on a map at a specific point.
-    /// @param mapHitbox The map to check.
-    /// @param mapX The x coordinate on the map to check.
-    /// @param mapY The y coordinate on the map to check.
-    /// @return The ARGB color of the pixel.
-    public static int getColorAtPoint(Image mapHitbox, int mapX, int mapY) {
-        int color;
-        if (mapX >= 0 && mapX < mapHitbox.getWidth() && mapY >= 0 && mapY < mapHitbox.getHeight()) {
-            color = mapHitbox.getPixelReader().getArgb(mapX, mapY);
-        } else {
-            color = 0;
-        }
-        return color;
-    }
 }
