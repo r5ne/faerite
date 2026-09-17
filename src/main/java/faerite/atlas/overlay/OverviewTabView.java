776d5b3a21daa52ce910a7b580934f087e9ffad9
r5ne <renetimantsev@gmail.com>
2026-09-17 02:12:13 +0100

    Created a class for each separate view.

---
+++ src/main/java/faerite/atlas/overlay/OverviewTabView.java
@@ -0,0 +1,13 @@
+package faerite.atlas.overlay;
+
+
+import faerite.atlas.AtlasViewModel;
+import javafx.scene.layout.VBox;
+
+public class OverviewTabView extends VBox {
+    AtlasViewModel viewModel;
+
+    public OverviewTabView(AtlasViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+}
