915cf7db12ab9cd562b62ea943a12695833393da
r5ne <renetimantsev@gmail.com>
2026-09-17 14:49:32 +0100

    Fixed not adding label to children. Added number to text resolving for populationSection. Made section not show up if value is missing.

--- src/main/java/faerite/atlas/overlay/OverviewTabView.java
+++ src/main/java/faerite/atlas/overlay/OverviewTabView.java
@@ -2,6 +2,7 @@ package faerite.atlas.overlay;
 
 import faerite.atlas.AtlasViewModel;
 import faerite.atlas.map.RegionDataCache;
+import faerite.model.RegionDataModel;
 import faerite.model.RegionSelectionModel;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -15,12 +16,40 @@ public class OverviewTabView extends VBox {
     public OverviewTabView(AtlasViewModel viewModel) {
         this.viewModel = viewModel;
 
+        populationNumberLabel.getStyleClass().add("info-section-value");
+
         InfoSection populationSection = new InfoSection("Population:", populationNumberLabel);
+        populationSection.managedProperty().bind(populationSection.visibleProperty());
+
+        getChildren().addAll(populationSection);
     }
 
     public void updateLabels(RegionSelectionModel newRegion) {
         String id = newRegion != null ? newRegion.id() : viewModel.getActiveLayer().mapModel.id();
+        RegionDataModel regionData = RegionDataCache.get(id);
 
-        populationNumberLabel.setText(String.valueOf(RegionDataCache.get(id).population()));
+        updatePopulationLabel(regionData);
+    }
+
+    private void updatePopulationLabel(RegionDataModel regionData) {
+        Long population = regionData.population();
+
+        if (population == null) {
+            populationNumberLabel.setVisible(false);
+            return;
+        }
+
+        populationNumberLabel.setVisible(true);
+        String populationString;
+
+        if (population >= 1000000000) {
+            populationString = population / 1000000000 + " billion";
+        } else if (population >= 1000000) {
+            populationString = population / 1000000 + " million";
+        } else {
+            populationString = String.format("%,d", population);
+        }
+
+        populationNumberLabel.setText(populationString);
     }
 }
