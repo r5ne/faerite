d502dc56a763e39041c5f07fef74532b5e347542
r5ne <renetimantsev@gmail.com>
2026-09-20 22:05:44 +0100

    Fixed UniformSpacerPane buttons not scaling when the window is smaller than the pane.

--- src/main/java/faerite/util/UniformSpacerPane.java
+++ src/main/java/faerite/util/UniformSpacerPane.java
@@ -22,6 +22,10 @@ public class UniformSpacerPane extends Region {
         }
 
         double totalButtonWidth = maxButtonWidth * children.size();
+        if (totalButtonWidth > width) {
+            maxButtonWidth = width / children.size();
+            totalButtonWidth = maxButtonWidth * children.size();
+        }
         double availableSpace = width - totalButtonWidth;
         double spacing = availableSpace / (children.size() + 1);
 
